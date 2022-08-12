10ec4d8fd9a25f121abf6bf258f4592b08fb0c80
Wendy Salinas-Reyes <wendy3salinas@gmail.com>
2022-08-12 14:08:07 -0700

    Add files via upload

---
+++ EdgeBrowserDemo/src/main/java/com/edgebrowser/EdgeBrowserDemo.java
@@ -0,0 +1,29 @@
+package com.edgebrowser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowserDemo {
+
+    public static void main(String args[]){
+        WebDriver driver = new EdgeDriver();
+
+        driver.get("http://google.com");
+
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.click();
+        searchField.sendKeys("Pluralsight");
+        searchField.sendKeys(Keys.ENTER);
+
+        WebElement imagesLink = driver.findElement(By.linkText("Images"));
+        imagesLink.click();
+
+        WebElement imageElement = driver.findElement(By.cssSelector("img[class ='rg_i Q4LuWd']"));
+        imageElement.click();
+        //driver.quit();
+
+    }
+}
